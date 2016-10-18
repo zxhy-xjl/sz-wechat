930b3c4b66e016ac7bcaf22d72cb3ad90f62fd60
Fachion <lyyglin@163.com>
2016-10-18 15:02:50 +0800

    1

--- src/main/java/com/sz/wechat/controller/FootprintController.java
+++ src/main/java/com/sz/wechat/controller/FootprintController.java
@@ -3,6 +3,11 @@
  */
 package com.sz.wechat.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.sz.wechat.entity.Footprint;
+import com.sz.wechat.entity.Menu;
 import com.sz.wechat.service.FootprintService;
 
 /**
@@ -27,10 +34,13 @@ public class FootprintController {
 	
 	
 	
-	@RequestMapping(value = "/insertLog",method = RequestMethod.POST)
-	public ModelAndView footprintGet(){
-		
+	@RequestMapping(value = "/toFootprint",method = RequestMethod.GET)
+	public ModelAndView footprintGet(HttpServletRequest request, HttpServletResponse response){
+		String openid = request.getParameter("openid");
 		ModelAndView modelAndView = new ModelAndView();
+		List<Footprint> footprintlist = this.footprintService.getFootprintByOpenid("oehpaw8_fgOEWtPk0S0gLidH60xg");
+		modelAndView.addObject("footprintList", footprintlist);
+		modelAndView.setViewName("/footprint");
 		return modelAndView;
 		
 	}
