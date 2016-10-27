b3f988309890bb9b0fb71f65d29b3553a674fe63
guanzheyuan <834155417@qq.com>
2016-10-27 11:43:35 +0800

    提交文件验证

--- src/main/java/com/sz/wechat/utils/ScanCodeUtils.java
+++ src/main/java/com/sz/wechat/utils/ScanCodeUtils.java
@@ -11,15 +11,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.sz.wechat.entity.Menu;
-
-import net.coobird.thumbnailator.Thumbnails;
 import net.sf.json.JSONObject;
 
 /**
